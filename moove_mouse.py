929a3948e009f39308d1a2a4e71604ef02861faf
ludo06000 <lc06000@gmail.com>
2024-04-16 22:25:08 +0200

    update moove_mouse

--- moove_mouse.py
+++ moove_mouse.py
@@ -2,16 +2,26 @@ import pyautogui
 import random
 import time
 
-print('Press Ctrl-C to quit.')
-print(__name__)
 
-try :
-    while True :
-        x = random.randint(600, 700)
-        y = random.randint(200, 600)
-        pyautogui.moveTo(x, y, 0.5)
-        time.sleep(60)
-except KeyboardInterrupt:
-    print('\n')
+def move_mouse_randomly(duration :int) -> None :
+    try :
+        while True :
+            x = random.randint(600, 700)
+            y = random.randint(200, 600)
+            pyautogui.moveTo(x, y, 0.5)
+            time.sleep(duration)
+    except KeyboardInterrupt:
+        print('\n')
 
+
+if __name__ == "__main__" :
+    print('Press Ctrl-C to quit.')
+    duration = int(input("Veuillez entréer le temps de pause : (defaut 60)") or 60)
+
+    try:
+        move_mouse_randomly(duration=duration)
+    except pyautogui.FailSafeException:
+        print('Échec de la fonction "FailSafe" de PyAutoGUI. La souris est proche du coin supérieur gauche de l\'écran.')
+    except Exception as e:
+        print(f'Une erreur s\'est produite : {e}')
 
